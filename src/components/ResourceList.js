0539279b4fb7e5273b019b7bc65491f4ee83f4b3
Roman Onishchenko <ro@rollncode.com>
2020-03-12 18:02:46 +0200

    init

---
+++ src/components/ResourceList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ResourceList extends React.Component {
+
+  render() {
+    return (
+      <div>Resource List</div>
+    );
+  }
+}
+
+export default ResourceList;
